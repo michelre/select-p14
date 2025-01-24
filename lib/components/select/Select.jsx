f129cfc58837772c4c04e01b0c73f755b9d97799
Rémi Michel <remi.michel38@gmail.com>
2025-01-24 18:05:25 +0100

    feat(): update style

--- lib/components/select/Select.jsx
+++ lib/components/select/Select.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Caret from './caret.svg'
+import PropTypes from 'prop-types';
+import './select.css'
 
-const Select = ({options, placeholder, onChange}) => {
- return <select onChange={(e) => onChange(e.target.value)}>
-    <option value=''>{placeholder}</option>
-    {options.map((o) => <option key={o.id} value={o.id}>{o.name}</option>)}
+const Select = ({options, onChange, defaultValue, placeholder}) => {
+   const [isListOpen, setIsListOpen] = useState(false);
 
- </select>   
+   const handleSelect = (e, value) => {
+      e.preventDefault()
+      onChange(value)
+      setIsListOpen(false); // ferme la liste après la sélection
+   }
+
+
+   return <div className="custom-select" onClick={() => setIsListOpen(!isListOpen)}>
+            
+   <div className="select-selected">
+     {defaultValue || placeholder}
+     <img src={Caret} alt="caret" className="select-caret" />
+   </div>
+   {isListOpen && (
+     <ul className="select-items">
+       {options.map((s) => <li key={s.id}>
+         <a href="" onClick={(e) => handleSelect(e, s.value)}>{s.value}</a>
+       </li>)}                
+     </ul>
+   )}
+   
+ </div>
 }
+
+Select.propTypes = {
+   options: PropTypes.array,
+   onChange: PropTypes.func,
+   defaultValue: PropTypes.string,
+   placeholder: PropTypes.string,
+}
+
 
 export default Select;
--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,10 @@ export default defineConfig({
 
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import libCss from "vite-plugin-libcss";
 
 export default defineConfig({
+  plugins: [libCss()],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.js'),
@@ -20,12 +22,13 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react'],
+      external: ['react', 'prop-types'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           react: 'React',
+          'prop-types': 'PropTypes'          
         },
       },
     },
